46dd9673cb29842f0123027783b41103efe1e2b7
MaximeMartrat <maximemartrat@outlook.fr>
2023-01-27 08:55:13 +0100

    maj minisnake

--- minisnake/app.js
+++ minisnake/app.js
@@ -1,9 +1,11 @@
+//je déclare mes variables en récupérant les différents objets du DOM
 let target = document.getElementById("target")
 let snake = document.getElementById("snake")
 let afficherScore = document.getElementById('score')
 let afficherLoose = document.getElementById('loose')
 let snakeX = 1;
 let snakeY = 1;
+//je fais apparaitre la target de façon aléatoire sur le plateau
 let targetX = Math.random()*90;
 let targetY = Math.random()*90;
 let winX = targetX;
@@ -11,6 +13,7 @@ let winY = targetY +10;
 let speed = 3;
 let score = 0;
 let loose = 0;
+//je cré la fonction pour bouger le carré avec les flèches du clavier
 function moveSnake() {
     document.addEventListener('keydown', function(event){
         if (event.key === "ArrowLeft"){
@@ -31,7 +34,8 @@ function moveSnake() {
         win();
         lose();
     }) 
-}            
+}
+//je crée une fonction pour changer la couleur de mon carré lorsqu'il est sur la target et incrémenter le compteur de victoires            
 function win(){
     if (snakeX <= winX+2 && snakeY <= winY+2 && snakeX >= winX-2 && snakeY >= winY-2) {
         snake.style.backgroundColor = "blue";
@@ -41,8 +45,10 @@ function win(){
         snake.style.backgroundColor = "black";
     }
 }
+//je créer une fonction pour incrémenter le compteur de défaites lorsque le carré sort de l'aire de jeu
 function lose(){
     if (snakeX <= 0 || snakeX >= 92 || snakeY <= 0 || snakeY >= 92) {
+        snake.style.backgroundColor = "red";
         loose +=1 ;
         afficherLoose.innerHTML = `Loose : ${loose}`;
     }
